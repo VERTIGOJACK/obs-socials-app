3aa29eadad3f564261f844bcb7a773d3e0fe7fda
VERTIGOJACK <vertigo.digital.design@gmail.com>
2024-06-11 10:32:10 +0200

    Added descriptions and enlarged scale

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
+// speedSeconds = How many seconds will a single item be shown on screen
+// delaySeconds = How many seconds will the app be inactive before looping through again
+// titleColor = Color of top text in hex, rgb, or alias (name of social media)
+// textColor = Color of bottom text in hex, rgb, or alias (name of your handle)
+
 const json = {
   speedSeconds: 5,
   delaySeconds: 0,
--- config/socials.js
+++ config/socials.js
@@ -1,6 +1,24 @@
+// add more or delete socials by adding more lines in the json array
+//place your images in the image folder
+
+// Template:
+
+// { name: "your name", handle: "your handle", image: "name of image in image folder.png" },
+
+// if you want the app to not be visible on the screen for a period of time
+// after looping through your socials, add an empty line at the end like so:
+
+// { name: "", handle: "", image: "" },
+
+// which is also the default behavior
+
 const json = [
   { name: "instagram", handle: "vertigoxdigital", image: "instagram.png" },
   { name: "spotify", handle: "VERTIGOJACK", image: "spotify.png" },
+  { name: "youtube", handle: "VERTIGOJACK", image: "yt.png" },
+  { name: "soundcloud", handle: "VERTIGOJACK", image: "sc.png" },
+  { name: "GitHub", handle: "VERTIGOJACK", image: "github.png" },
+  { name: "Website", handle: "Vertigodigital.se", image: "vdd.png" },
   { name: "", handle: "", image: "" },
 ];
 
